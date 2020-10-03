eb8b5dd9ef905669f56518a3afa55a95bded1060
Buch <hhccuubb@ukr.net>
2020-10-03 11:05:23 +0300

    Day4

--- script/generateGoodsPage.js
+++ script/generateGoodsPage.js
@@ -1,19 +1,61 @@
 import  { getData }  from "./getData.js";
+import userData from "./userData.js";
 
+const COUNTER = 6;
 
-const wishList = ['idd005', 'idd033', 'idd095', 'idd077'];
 
 const generateGoodsPage = () => {
-
     const mainHeader = document.querySelector('.main-header');
-    const goodsList = document.querySelector('.goods-list');
+
     const generateCards = (data) => {
+        const goodsList = document.querySelector('.goods-list');
+        console.log(goodsList);
         goodsList.textContent = '';
 
+        if(!data.length){
+            const  goods = document.querySelector('.goods');
+            goods.textContent = location.search === '?wishlist' ?
+                'Список желаний пуст' :
+                'По вашему запросу ничего не найдено';
+        }
+
         data.forEach(item => {
-            goodsList.insertAdjacentHTML('afterbegin', `
-            <li>${item.name}</li>`)
-        })
+            console.log(item);
+            const {name, count, description, id, img, price} = item;
+
+            goodsList.insertAdjacentHTML('afterbegin',
+                `
+                 <li class="goods-list__item">
+                <a class="goods-item__link" href="card.html#${id}">
+                <article class="goods-item">
+                <div class="goods-item__img">
+                <img src=${img[0]}
+                 ${img[1] ? `data-second-image=${img[1]}` : ''}>
+                </div>
+               ${count > COUNTER ?`<p class="goods-item__new">Новинка</p>` : ''}
+               ${!count ? `<p class="goods-item__new">Нет в наличии</p>` : ''}
+               <h3 class="goods-item__header">${name}</h3>
+                <p class="goods-item__description">${description}</p>
+                <p class="goods-item__price">
+               <span class="goods-item__price-value">${price}</span>
+              <span class="goods-item__currency"> ₽</span>
+                </p>
+                 ${count ? `<button class="btn btn-add-card" aria-label="Добравить в корзину" data-idd="${id}"></button>` : ''}
+                
+                </article>
+                </a>
+                </li>
+                `);
+        });
+        goodsList.addEventListener('click', e => {
+
+            const btnAddCard = e.target.closest('.btn-add-card');
+            if(btnAddCard){
+                e.preventDefault();
+                userData.cartList = btnAddCard.dataset.idd;
+                console.log(userData.cartList);
+            }
+        });
     };
 
     if(location.pathname.includes('goods') && location.search){
@@ -25,12 +67,14 @@ const generateGoodsPage = () => {
             getData.search(value, generateCards);
             mainHeader.textContent = `Поиск: ${value}`;
         }else if(prop === 'wishlist'){
-            getData.wishList(wishList, generateCards);
+            getData.wishList(userData.wishList, generateCards);
             mainHeader.textContent = `Список желаний`;
         } else if(prop === 'cat' || prop === 'subcat'){
             getData.category(prop, value, generateCards);
             mainHeader.textContent = value;
         }
     }
+
+
 };
 export default generateGoodsPage;
--- script/loadData.js
+++ script/loadData.js
@@ -1,30 +1,8 @@
 import { getData } from "./getData.js";
 
-
-
-
-
-const cartList = [
-    {
-        id: 'idd015',
-        count: 3
-    },
-    {
-        id: 'idd043',
-        count: 1
-    },
-    {
-        id: 'idd065',
-        count: 2
-    }
-];
-
+// const cartList = [];
 export const loadData = () => {
 
-
-    if(location.hash){
-        getData.item(location.hash.substring(1), (data) => console.log(data));
-    }
     if(location.pathname.includes('cart')){
         getData.cart(cartList, (data) => console.log(data));
     }
